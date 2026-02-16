88ad214e91b51339e1eb1bb30d66165e77e97d22
joaocarlinhosfm <joaocarlinhos.fm@gmail.com>
2026-02-16 20:33:00 +0000

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'stock-v13';
+const CACHE_NAME = 'stock-v14';
 const ASSETS = ['./', './index.html', './style.css', './app.js', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', (e) => {
