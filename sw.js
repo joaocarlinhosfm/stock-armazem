2f1c11d043b37d55e94872bc47e73ea64ab5bfbc
joaocarlinhosfm <joaocarlinhos.fm@gmail.com>
2026-02-19 21:56:31 +0000

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'stock-v18';
+const CACHE_NAME = 'stock-v19';
 const ASSETS = ['./', './index.html', './style.css', './app.js', './manifest.json'];
 
 self.addEventListener('install', (e) => {
@@ -14,5 +14,6 @@ self.addEventListener('fetch', (e) => {
     if (e.request.url.includes('firebaseio.com')) return;
     e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
 });
+
 
 
